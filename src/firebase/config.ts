a33d8c130ac25a1b73042c1cb8354b55d4126aea
Marcelo Vergeti <lelovergeti@gmail.com>
2026-03-17 18:55:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-940859647-87d98",
+  "appId": "1:909507448155:web:6ec51b98bbf2c31dbfde7e",
+  "apiKey": "AIzaSyCDmPBuiip6R6wJkdnUkivtXJoPChNopIE",
+  "authDomain": "studio-940859647-87d98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "909507448155"
+};
